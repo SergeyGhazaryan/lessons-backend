f6ae31451b0f0c121de56e9d881006ca7f2e70b3
SergeyGhazaryan <sergey.ghazaryan2000@gmail.com>
2021-08-06 00:13:40 +0400

    updated migrations

--- src/Lessons-api.Data/Migrations/20210805201228_deleteTeacherStudentRels.Designer.cs
+++ src/Lessons-api.Data/Migrations/20210805201228_deleteTeacherStudentRels.Designer.cs
@@ -3,14 +3,16 @@ using Lessons_api.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Lessons_api.Data.Migrations
 {
     [DbContext(typeof(LessonsContext))]
-    partial class LessonsContextModelSnapshot : ModelSnapshot
+    [Migration("20210805201228_deleteTeacherStudentRels")]
+    partial class deleteTeacherStudentRels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Lessons-api.Data/Migrations/20210805201228_deleteTeacherStudentRels.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lessons_api.Data.Migrations
+{
+    public partial class deleteTeacherStudentRels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TeacherStudentRels");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TeacherStudentRels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    TeacherId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeacherStudentRels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TeacherStudentRels_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TeacherStudentRels_Teachers_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teachers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherStudentRels_StudentId",
+                table: "TeacherStudentRels",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherStudentRels_TeacherId",
+                table: "TeacherStudentRels",
+                column: "TeacherId");
+        }
+    }
+}
